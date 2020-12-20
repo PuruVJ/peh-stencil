be661d828e1a6927b1b6f1a46b760799c110b938
Puru Vijay <devpuruvj@gmail.com>
2020-12-20 14:22:55 +0530

    Capital

--- src/components/new-location/new-location.tsx
+++ src/components/new-location/new-location.tsx
@@ -8,7 +8,10 @@ import { Component, h } from "@stencil/core";
 export class NewLocation {
   render = () => (
     <section class="container">
-      Puru Eye Hospital, Mansarovar is moving to a new location <br />
+      <span class="capital">
+        Puru Eye Hospital, Mansarovar is moving to a new location{" "}
+      </span>{" "}
+      <br />
       Address: <b>76/7, Shipra Path, Opp Landscape Park, Mansarovar</b> <br />
       <a
         href="https://goo.gl/maps/X9juPNBUfEuirnLNA"
